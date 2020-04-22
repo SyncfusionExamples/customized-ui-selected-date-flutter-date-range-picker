00e67747dcf3a3f246a0ef0d58d5de5b7ae7282b
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-04-22 18:18:02 +0530

    sample added

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(SelectedCellCustomization());
+
+class SelectedCellCustomization extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SelectedCell(),
+    );
+  }
+}
+
+class SelectedCell extends StatefulWidget {
+  @override
+  _SelectedCellState createState() => _SelectedCellState();
+}
+
+class _SelectedCellState extends State<SelectedCell> { 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Card(
+      margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+      child: SfDateRangePicker(
+        view: DateRangePickerView.month,
+        selectionShape: DateRangePickerSelectionShape.rectangle,
+        monthCellStyle: DateRangePickerMonthCellStyle(
+          selectionTextStyle:
+              TextStyle(color: Colors.red,decoration: TextDecoration.underline,),
+          selectionColor: Colors.amber,
+          textStyle: TextStyle(fontSize: 15,color: Colors.black),
+
+        ),
+      ),
+    ));
+  }
+}
